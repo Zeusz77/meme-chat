5a31df295afe2dace5b5b5883ae1a439bacfcdaf
Zeusz77 <aron.franta@gmail.com>
2022-10-13 18:22:13 +0200

    Make DB connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express(); 
+const uri = "mongodb+srv://admin:admin@meme-chat.wicw97u.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri).then((dbo)=>{
+    console.log("DB connected")
+    },(err)=>{
+    console.log(err)
+});
+
 
 app.get('/', (req, res)=>{
-    res.status(200).send('Hi!');
+    res.status(200).send('Hello World!');
 });
 
 const port = 3001;
---
+++ src/db/connection.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+const db = "mongodb+srv://admin:admin@meme-chat.wicw97u.mongodb.net/?retryWrites=true&w=majority";
