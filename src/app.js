1953ec9bbc44ec10ab7c8084a67a199142a7b1a6
Zeusz77 <aron.franta@gmail.com>
2022-10-13 18:30:30 +0200

    db/connection

--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
 
 const app = express(); 
 const uri = "mongodb+srv://admin:admin@meme-chat.wicw97u.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.connect(uri).then((dbo)=>{
-    console.log("DB connected")
-    },(err)=>{
-    console.log(err)
-});
-
 
 app.get('/', (req, res)=>{
     res.status(200).send('Hello World!');
