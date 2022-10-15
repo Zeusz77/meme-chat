57c73ae0c412c774788db264f7f6f19c896c2539
Zeusz77 <aron.franta@gmail.com>
2022-10-15 17:00:20 +0200

    Login

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://zus:zus@meme-chat.wicw97u.mongodb.net/?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://zus:zus@meme-chat.wicw97u.mongodb.net/?retryWrites=true&w=majority',
+    key: '12345678'
 }
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const User = require('../../models/User');
+const { response } = require('express');
 
 router.get('/test', (req, res)=> {
     res.json({msg: 'test message'});
@@ -36,5 +39,31 @@ router.post('/register', (req, res) => {
         }
     })
 });
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+    .then(user => {
+        if(!user) return res.status(404).json({email: 'User not found'});
+
+        bcrypt.compare(password, user.password)
+            .then(isMatch => {
+                if(isMatch) {
+                    const payload = {id: user.id, email: user.email, handle: user.handle}
+
+                    jwt.sign(payload, keys.key, {expiresIn: 3600}, (err, token)=>{
+                        res.json({
+                            success: true,
+                            token: 'memechat-auth:' + token
+                        })
+                    });
+                }
+                else{ 
+                    return res.status(400).json({password: 'Password mismatch'});
+            }})
+    })
+})
 
 module.exports = router;
