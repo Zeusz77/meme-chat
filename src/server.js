8a9aee6a2b65f4396096bac48940f9dbbf1f7bf3
Zeusz77 <aron.franta@gmail.com>
2022-10-19 00:11:05 +0200

    Client Setup

--- src/server.js
+++ src/server.js
@@ -30,6 +30,6 @@ app.get(`/`, (req, res)=> res.send('Hello!'));
 
 app.use('/api/users', users);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, ()=> console.log(`listening on port: ${port}`));
