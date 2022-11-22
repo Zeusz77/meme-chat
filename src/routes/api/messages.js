a3142eba86fb34f26c707b68d2a74ce99d3583a1
Zeusz77 <aron.franta@gmail.com>
2022-11-23 00:44:06 +0100

    Update messages.js

--- src/routes/api/messages.js
+++ src/routes/api/messages.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+const Jimp = require('jimp');
 
 router.get('/test', (req, res)=> {
     res.json({msg: 'test message'});
@@ -11,9 +12,22 @@ router.get('/test', (req, res)=> {
 router.get('/chat/:chatId', (req, res) => {
     Message.find({chatId: req.params.chatId})
     .then(messages => {
-        res.json(messages);
+        // return all messages in order of creation from newest to oldest
+        res.json(messages.reverse());
     })
     .catch(err => res.status(404).json({nomessagesfound: 'No messages found'}));
+});
+
+// Send a message to a chatroom
+router.post('/chat/:chatId', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const newMessage = new Message({
+        template : req.body.template,
+        text : req.body.text,
+        chat : req.params.chat,
+        user : req.user.id
+    });
+
+    newMessage.save().then(message => res.json(message));
 });
 
 module.exports = router;
