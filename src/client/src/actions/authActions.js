1ab593af980ef3e1c10e3e955fcde283a79c9a44
Zeusz77 <aron.franta@gmail.com>
2022-11-27 22:20:28 +0100

    Sex

--- src/client/src/actions/authActions.js
+++ src/client/src/actions/authActions.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { GET_ERRORS } from "./types";
 
-export const registerUser = (userData) => dispatch => {
+export const registerUser = (userData, history) => dispatch => {
+
+    const navigate = useNavigate();
+
     axios.post('/api/users/register', userData)
-    .then(res => console.log(res.data))
+    .then(res => navigate('/login'))
     .catch(err => 
         dispatch({
             type: GET_ERRORS,
