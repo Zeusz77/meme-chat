9d8b88af2fe8309bc1421d885f9c346544ae305d
Zeusz77 <aron.franta@gmail.com>
2022-12-24 00:09:54 +0100

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,41 +20,44 @@ export const Navbar = () => {
 
     return (
         <nav className="navbar navbar-light bg-light">
-            <div className="container-fluid">
+        <div className="container-fluid">
+            <div className="container" id="navbarNav">
+            <div className="row">
+                <div className="col-auto">
                 <Link className="navbar-brand" to="/">Meme-chat</Link>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        </li>
+                </div>
+                <div className="col-auto ml-auto">
+                <ul className="navbar-nav flex-row">
+                    <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                    </li>
 
-                        {isLoggedIn ? (
-                            <Fragment>
-                                <li className="nav-item">
-                                    <a onClick={handleClick} className="nav-link" href="#logout">
-                                        Logout {user.handle}
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/addTemplate">Add new template</Link>
-                                </li>
-                            </Fragment>
-                        ) : (
-                            <Fragment>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/login">Login</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/register">Register</Link>
-                                </li>
-                            </Fragment>
-                        )}
-                        
-                    </ul>
+                    {isLoggedIn ? (
+                    <Fragment>
+                        <li className="nav-item mx-2">
+                        <a onClick={handleClick} className="nav-link" href="#logout">
+                            Logout {user.handle}
+                        </a>
+                        </li>
+                        <li className="nav-item mx-2">
+                        <Link className="nav-link" to="/addTemplate">Add new template</Link>
+                        </li>
+                    </Fragment>
+                    ) : (
+                    <Fragment>
+                        <li className="nav-item mx-2">
+                        <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item mx-2">
+                        <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </Fragment>
+                    )}
+                </ul>
                 </div>
             </div>
+            </div>
+        </div>
         </nav>
-
-
     );
 };
